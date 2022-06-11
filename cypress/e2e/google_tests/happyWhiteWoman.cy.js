bb6f9385d3f0f3e72006c0228000063edf11140b
karolmacheta <karol.macheta@gmail.com>
2022-06-11 09:51:50 +0200

    wip

---
+++ cypress/e2e/google_tests/happyWhiteWoman.cy.js
@@ -0,0 +1,20 @@
+describe('Google Search', () => {
+  it('loads search page', () => {
+    cy.visit('https://www.google.com');
+    cy.get('button div')
+    .contains('Zgadzam się')
+    .scrollIntoView()
+    .click();
+  });
+
+  it('searches for `remarkablemark`', () => {
+    cy.get('input[name="q"]').type('remarkablemark{enter}');
+    cy.realPress('Enter');
+  });
+
+  it('gets first search result', () => {
+    cy.get('#search a')
+      .invoke('attr', 'href')
+      .then((href) => console.log(href));
+  });
+});
